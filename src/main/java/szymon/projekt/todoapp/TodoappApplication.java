357cee95f8485a6895c2e110acf41becce1b13fc
szywski27@gmail.com <LOki6262>
2021-08-25 18:11:18 +0200

    Initial commit

---
+++ src/main/java/szymon/projekt/todoapp/TodoappApplication.java
@@ -0,0 +1,13 @@
+package szymon.projekt.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappApplication.class, args);
+	}
+
+}
